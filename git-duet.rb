23650d4ce5d6063129eea84f450c89e9abaecf4a
Jesse Szwedko <jesse.szwedko@gmail.com>
2015-08-20 20:43:27 -0700

    Update to 0.3.0

--- git-duet.rb
+++ git-duet.rb
@@ -1,13 +1,13 @@
 class GitDuet < Formula
   homepage 'https://github.com/git-duet/git-duet'
-  url 'https://github.com/git-duet/git-duet/releases/download/0.2.0/darwin_amd64.tar.gz'
-  sha256 'cdf45e16a4bb027c7a17ad010ab2633a77613578b44c2550a70dd969e5f9da23'
-  version '0.2.0'
+  url 'https://github.com/git-duet/git-duet/releases/download/0.3.0/darwin_amd64.tar.gz'
+  sha256 '0669e056f0a8c3e7e8e96bf8a7a9861c256a84932bee6d103a68ad3398a2b2a5'
+  version '0.3.0'
 
   depends_on :arch => :x86_64
 
   def install
-    %w( git-duet git-duet-commit git-duet-install-hook git-duet-pre-commit git-solo).each do |exe|
+    %w( git-duet git-duet-commit git-duet-revert git-duet-install-hook git-duet-pre-commit git-solo).each do |exe|
       bin.install exe
     end
   end
