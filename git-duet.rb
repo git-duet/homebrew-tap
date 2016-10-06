052338fe42f3ad49897388185bf0ba640bef4e20
Jesse Szwedko <jesse.szwedko@gmail.com>
2016-10-05 22:34:41 -0700

    Update git-duet version to 0.3.1

--- git-duet.rb
+++ git-duet.rb
@@ -1,8 +1,8 @@
 class GitDuet < Formula
   homepage 'https://github.com/git-duet/git-duet'
-  url 'https://github.com/git-duet/git-duet/releases/download/0.3.0/darwin_amd64.tar.gz'
-  sha256 '0669e056f0a8c3e7e8e96bf8a7a9861c256a84932bee6d103a68ad3398a2b2a5'
-  version '0.3.0'
+  url 'https://github.com/git-duet/git-duet/releases/download/0.3.1/darwin_amd64.tar.gz'
+  sha256 'af2e048cda79606d3c90a89ae16a1f0d99866b43daabf01bda340f019a681617'
+  version '0.3.1'
 
   depends_on :arch => :x86_64
 
