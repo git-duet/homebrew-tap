b2950e0ceada4b3f325ac5e2e3b2b95e1e4970c1
Dennis Ideler <ideler.dennis@gmail.com>
2016-11-27 16:26:19 -0400

    Add Linux support

--- git-duet.rb
+++ git-duet.rb
@@ -1,7 +1,12 @@
 class GitDuet < Formula
   homepage 'https://github.com/git-duet/git-duet'
-  url 'https://github.com/git-duet/git-duet/releases/download/0.3.1/darwin_amd64.tar.gz'
-  sha256 'af2e048cda79606d3c90a89ae16a1f0d99866b43daabf01bda340f019a681617'
+  if OS.mac?
+    url 'https://github.com/git-duet/git-duet/releases/download/0.3.1/darwin_amd64.tar.gz'
+    sha256 'af2e048cda79606d3c90a89ae16a1f0d99866b43daabf01bda340f019a681617'
+  elsif OS.linux?
+    url 'https://github.com/git-duet/git-duet/releases/download/0.3.1/linux_amd64.tar.gz'
+    sha256 'd704f50ad4e4d10b2d99de82998247548a4f72c269f5e41d076baf4454e8c02f'
+  end
   version '0.3.1'
 
   depends_on :arch => :x86_64
