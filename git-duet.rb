28403c39cd57b2ebaf5db5caa9ef2ebbb2b9fe4a
Dennis Ideler <ideler.dennis@gmail.com>
2016-11-27 16:39:31 -0400

    Prefer double quoted strings
    As suggested by homebrew's audit tests.

--- git-duet.rb
+++ git-duet.rb
@@ -1,14 +1,14 @@
 class GitDuet < Formula
   desc "Pairing tool for Git"
-  homepage 'https://github.com/git-duet/git-duet'
+  homepage "https://github.com/git-duet/git-duet"
   if OS.mac?
-    url 'https://github.com/git-duet/git-duet/releases/download/0.3.1/darwin_amd64.tar.gz'
-    sha256 'af2e048cda79606d3c90a89ae16a1f0d99866b43daabf01bda340f019a681617'
+    url "https://github.com/git-duet/git-duet/releases/download/0.3.1/darwin_amd64.tar.gz"
+    sha256 "af2e048cda79606d3c90a89ae16a1f0d99866b43daabf01bda340f019a681617"
   elsif OS.linux?
-    url 'https://github.com/git-duet/git-duet/releases/download/0.3.1/linux_amd64.tar.gz'
-    sha256 'd704f50ad4e4d10b2d99de82998247548a4f72c269f5e41d076baf4454e8c02f'
+    url "https://github.com/git-duet/git-duet/releases/download/0.3.1/linux_amd64.tar.gz"
+    sha256 "d704f50ad4e4d10b2d99de82998247548a4f72c269f5e41d076baf4454e8c02f"
   end
-  version '0.3.1'
+  version "0.3.1"
 
   depends_on :arch => :x86_64
 
@@ -19,6 +19,6 @@ class GitDuet < Formula
   end
 
   test do
-    system 'git duet -h'
+    system "git duet -h"
   end
 end
