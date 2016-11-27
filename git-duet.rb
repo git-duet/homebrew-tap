f9ae93ceb7b76bd72d98c9ca84583e47571dc208
Dennis Ideler <ideler.dennis@gmail.com>
2016-11-27 16:44:44 -0400

    Version should appear before checksum
    As suggested by homebrew's audit tests.

--- git-duet.rb
+++ git-duet.rb
@@ -1,6 +1,7 @@
 class GitDuet < Formula
   desc "Pairing tool for Git"
   homepage "https://github.com/git-duet/git-duet"
+  version "0.3.1"
   if OS.mac?
     url "https://github.com/git-duet/git-duet/releases/download/0.3.1/darwin_amd64.tar.gz"
     sha256 "af2e048cda79606d3c90a89ae16a1f0d99866b43daabf01bda340f019a681617"
@@ -8,7 +9,6 @@ class GitDuet < Formula
     url "https://github.com/git-duet/git-duet/releases/download/0.3.1/linux_amd64.tar.gz"
     sha256 "d704f50ad4e4d10b2d99de82998247548a4f72c269f5e41d076baf4454e8c02f"
   end
-  version "0.3.1"
 
   depends_on :arch => :x86_64
 
