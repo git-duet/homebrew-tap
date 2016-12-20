fa6282ea2f460bc1bf0d56fb3ae726caae5a53fa
Kurtis Seebaldt <kseebaldt@pivotal.io>
2016-12-20 11:30:23 -0600

    Update tap for version 0.5.0
    Signed-off-by: Jeremy Gustine <jgustine@pivotal.io>

--- git-duet.rb
+++ git-duet.rb
@@ -1,8 +1,8 @@
 class GitDuet < Formula
   homepage 'https://github.com/git-duet/git-duet'
-  url 'https://github.com/git-duet/git-duet/releases/download/0.3.1/darwin_amd64.tar.gz'
-  sha256 'af2e048cda79606d3c90a89ae16a1f0d99866b43daabf01bda340f019a681617'
-  version '0.3.1'
+  version '0.5.0'
+  url "https://github.com/git-duet/git-duet/releases/download/#{version}/darwin_amd64.tar.gz"
+  sha256 "88050ceb98480a7917106180c4d81764f94db5719ad3b458b90ac7af6cee9849"
 
   depends_on :arch => :x86_64
 
