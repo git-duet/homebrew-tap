aabb50d48c2764c7370588b61fc8d1d3a7b6cb18
Jesse Szwedko <jesse.szwedko@gmail.com>
2017-04-24 10:51:26 -0700

    Bump to 0.5.2

--- git-duet.rb
+++ git-duet.rb
@@ -1,13 +1,13 @@
 class GitDuet < Formula
   desc "Pairing tool for Git"
   homepage "https://github.com/git-duet/git-duet"
-  version "0.5.1"
+  version "0.5.2"
   if OS.mac?
-    url "https://github.com/git-duet/git-duet/releases/download/0.5.1/darwin_amd64.tar.gz"
-    sha256 "28d90acd34291fe98505f9f808cbd36d2c8cc2828a1ec6da7c699058468d2e86"
+    url "https://github.com/git-duet/git-duet/releases/download/0.5.2/darwin_amd64.tar.gz"
+    sha256 "aa2aeb451a989bf86504cc569b1a2f53a9971711c6dfb28cda7ae4f50c6334a0"
   elsif OS.linux?
-    url "https://github.com/git-duet/git-duet/releases/download/0.5.1/linux_amd64.tar.gz"
-    sha256 "0762f5845928feb04a7e43de55330feae2fd4a2f280a964e818aa336706d924d"
+    url "https://github.com/git-duet/git-duet/releases/download/0.5.2/linux_amd64.tar.gz"
+    sha256 "936fa5f31e94112f1499ac950320f8e7279e38530b56ed7615b3280514763a3c"
   end
 
   depends_on :arch => :x86_64
