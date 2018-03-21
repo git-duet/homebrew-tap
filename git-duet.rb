2cccc337b72de89d9339817a58aeb706d9217a6e
Florian <florian.schmidt.welzow@t-online.de>
2018-03-21 08:03:41 +0100

    Bump to version 0.6.0
    This will mainly allow to use the experimental feature to use the Co-Authored-By trailer instead of the Signed-Off-By.

--- git-duet.rb
+++ git-duet.rb
@@ -1,19 +1,19 @@
 class GitDuet < Formula
   desc "Pairing tool for Git"
   homepage "https://github.com/git-duet/git-duet"
-  version "0.5.2"
+  version "0.6.0"
   if OS.mac?
-    url "https://github.com/git-duet/git-duet/releases/download/0.5.2/darwin_amd64.tar.gz"
-    sha256 "aa2aeb451a989bf86504cc569b1a2f53a9971711c6dfb28cda7ae4f50c6334a0"
+    url "https://github.com/git-duet/git-duet/releases/download/0.6.0/darwin_amd64.tar.gz"
+    sha256 "412ad30f5c3e46c9dedcd12a237691e6f569e97f381b2fcfda1297c671c54a4f"
   elsif OS.linux?
-    url "https://github.com/git-duet/git-duet/releases/download/0.5.2/linux_amd64.tar.gz"
-    sha256 "936fa5f31e94112f1499ac950320f8e7279e38530b56ed7615b3280514763a3c"
+    url "https://github.com/git-duet/git-duet/releases/download/0.6.0/linux_amd64.tar.gz"
+    sha256 "0ce4adebb0aa00df4b3ba28e634f0528e3068abc0e977d1a976ba4357b74b4ac"
   end
 
   depends_on :arch => :x86_64
 
   def install
-    %w[git-duet git-duet-commit git-duet-revert git-duet-install-hook git-duet-merge git-duet-pre-commit git-solo].each do |exe|
+    %w[git-duet git-duet-commit git-duet-revert git-duet-install-hook git-duet-merge git-duet-pre-commit git-duet-prepare-commit-msg git-solo].each do |exe|
       bin.install exe
     end
   end
