198e96aa6ddd3f5a80d432048c7b5e502811e180
Jesse Szwedko <jesse.szwedko@gmail.com>
2019-04-27 22:33:36 +0200

    Update to 0.7.0

--- git-duet.rb
+++ git-duet.rb
@@ -1,13 +1,13 @@
 class GitDuet < Formula
   desc "Pairing tool for Git"
   homepage "https://github.com/git-duet/git-duet"
-  version "0.6.0"
+  version "0.7.0"
   if OS.mac?
-    url "https://github.com/git-duet/git-duet/releases/download/0.6.0/darwin_amd64.tar.gz"
-    sha256 "412ad30f5c3e46c9dedcd12a237691e6f569e97f381b2fcfda1297c671c54a4f"
+    url "https://github.com/git-duet/git-duet/releases/download/0.7.0/darwin_amd64.tar.gz"
+    sha256 "22980a514f7fbb0843d726c00db9f49fda5c2d5e5a3fcfd3b185f4d3eb8e6851"
   elsif OS.linux?
-    url "https://github.com/git-duet/git-duet/releases/download/0.6.0/linux_amd64.tar.gz"
-    sha256 "0ce4adebb0aa00df4b3ba28e634f0528e3068abc0e977d1a976ba4357b74b4ac"
+    url "https://github.com/git-duet/git-duet/releases/download/0.7.0/linux_amd64.tar.gz"
+    sha256 "97f7e5dd6f319c2ba56a28a937b1a3af4f66c06b9d7059633046fac5d1e1b93c"
   end
 
   depends_on :arch => :x86_64
