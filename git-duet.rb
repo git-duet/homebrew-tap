78436020fc1f0609f70a9108dbaeb94c967070ef
Joseph Palermo <jpalermo@pivotal.io>
2023-12-05 22:34:43 -0800

    Update SHAs for 0.10.0

--- git-duet.rb
+++ git-duet.rb
@@ -9,14 +9,14 @@ class GitDuet < Formula
       sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
     elsif Hardware::CPU.arm?
       url "https://github.com/git-duet/git-duet/releases/download/0.10.0/darwin_arm64.tar.gz"
-      sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+      sha256 "edf84e298029571b5f377993abb50d3c553cdb953cc3ff1acf136802d5e40165"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/git-duet/git-duet/releases/download/0.10.0/linux_amd64.tar.gz"
-      sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+      sha256 "c6db0838dddbb76604bdf02dc086dd0fb105096b4b468438a16cf4bcb40ad6f0"
     end
   end
 
